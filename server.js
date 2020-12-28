5943e7a48a4b9c291c7c64a3daf77f0d5932b277
Quang Tuyen DINH <qtuyen43@gmail.com>
2020-12-28 13:33:31 +0100

    Finished components books and authors

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
+// if(process.env.NODE_ENV !== 'production'){
+//   require('dotenv').parse()
+// }
+require("dotenv").config();
+
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-const indexRouter = require('./routes/index')
+const bodyParser = require('body-parser');
+
+const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
+
 app.use(expressLayouts);
 app.use(express.static('public'));
-app.use('/', indexRouter)
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
+app.use('/', indexRouter);
+app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to database'));
 
 app.listen(process.env.PORT || 3000, () => console.log("Server running on port 3000"));
